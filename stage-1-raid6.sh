b1a69ebb14d9d104c8c130cdeed19219ccde5da9
patrushevna <softomes@yandex.ru>
2019-11-18 18:45:32 +1000

    LessonTwo

---
+++ stage-1-raid6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
+mdadm --create --verbose /dev/md0 -l 6 -n 5 /dev/sd{b,c,d,e,f}
+mkdir /etc/mdadm
+cp /dev/null /etc/mdadm/mdadm.conf
+echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
+mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
+
